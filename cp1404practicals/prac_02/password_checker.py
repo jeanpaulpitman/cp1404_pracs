4b7f7c935d179e46ee0c8fa1dd0674ccd15e61f1
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-08-07 10:43:10 +1000

    Prac02
    save password checker

--- cp1404practicals/prac_02/password_checker.py
+++ cp1404practicals/prac_02/password_checker.py
@@ -2,34 +2,37 @@
 CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
-
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+MIN_LOWER = 2
+MIN_UPPER = 2
+MIN_DIGIT = 2
+MIN_SPECIAL = 1
+MIN_LENGTH = 7
+MAX_LENGTH = 10
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
     """Program to get and check a user's password."""
     print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
+    print("Your password must be between {} and {} characters, and contain:".format(MIN_LENGTH, MAX_LENGTH))
+    print("\t{} or more uppercase characters".format(MIN_UPPER))
+    print("\t{} or more lowercase characters".format(MIN_LOWER))
+    print("\t{} or more numbers".format(MIN_DIGIT))
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print("\tand {} or more special characters: {}".format(MIN_SPECIAL, SPECIAL_CHARACTERS))
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
-    print("Your " + str(
-        len(password)) + " character password is valid: " + password)
+    print("Your {} character password is valid: {}".format(len(password), password))
 
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
     # TODO: if length is wrong, return False
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
 
     count_lower = 0
     count_upper = 0
@@ -37,12 +40,24 @@ def is_valid_password(password):
     count_special = 0
     for char in password:
         # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if char.islower():
+            count_lower += 1
+        elif char.isupper():
+            count_upper += 1
+        elif char.isdigit():
+            count_digit += 1
+        elif char in SPECIAL_CHARACTERS:
+            count_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
+    if count_lower < MIN_LOWER or count_upper < MIN_UPPER or count_digit < MIN_DIGIT:
+        return False
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special < MIN_SPECIAL:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
