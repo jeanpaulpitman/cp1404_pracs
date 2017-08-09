4ba8256a46fa8d10d2321c1f5b262e316ed0dce4
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-08-09 17:15:21 +1000

    add pass activity
    add reverse string reader for pass tonight

---
+++ cp1404practicals/prac_02/pass_2_2.py
@@ -0,0 +1,11 @@
+"""
+Pass Session activity
+Create a program that prints each letter in the string "Trick or treat" on a new line and backwards
+"""
+
+print("Welcome to JP's backwards string reader 9001")
+my_string = "Trick or Treat"
+for kind in reversed(my_string):
+    print(kind)
+
+
