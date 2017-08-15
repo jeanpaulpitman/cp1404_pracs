fe2544b3d7761df3ad2882cba61065c1c1335ad0
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-08-15 20:41:56 +1000

    add functions to name_thing

--- cp1404practicals/prac_03/name_thing.py
+++ cp1404practicals/prac_03/name_thing.py
@@ -1,8 +1,28 @@
 def main():
+    user_name = get_name()
+    print(return_characters(user_name, get_frequency()))
+
+
+def return_characters(user_name, frequency):
+    return user_name[::frequency]
+
+
+def get_name():
     user_name = ""
     while user_name == "":
         print("Please enter your name")
         user_name = input("Name: ")
-    print(user_name[::2])
+    return user_name
+
+
+def get_frequency():
+    letter_frequency = 0
+    while letter_frequency == 0:
+        try:
+            print("Please enter letter frequency")
+            letter_frequency = int(input("Frequency: "))
+        except ValueError:
+            print("That is not a number, please enter a number")
+    return letter_frequency
 
 main()
