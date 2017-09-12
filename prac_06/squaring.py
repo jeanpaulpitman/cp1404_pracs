8524c3981ad8780238d2c331bca7c3765af86b9b
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-09-12 22:23:50 +1000

    :rocket: squaring.py complete

--- prac_06/squaring.py
+++ prac_06/squaring.py
@@ -8,9 +8,6 @@ Started 13/10/2015
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.graphics import Color
-
-
 
 __author__ = 'Lindsay Ward'
 
@@ -19,7 +16,7 @@ class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (2000, 1000)
+        Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
@@ -27,7 +24,7 @@ class SquareNumberApp(App):
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
         result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        self.root.ids.output_label.text = (str(result))
 
 
 SquareNumberApp().run()
