a40c6e85cf879eb039f84d6d9d356a7a4cfc5972
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-10-25 11:05:26 +1000

    :rocket: recursion activity part 2 complete

--- prac_10/recursion.py
+++ prac_10/recursion.py
@@ -13,17 +13,18 @@ def do_it(n):
 
 # TODO: 1. write down what you think the output of this will be,
 # TODO: 2. use the debugger to step through and see what's actually happening
-print(do_it(7))
+# print(do_it(7))
 
 
 def do_something(n):
     """Print the squares of positive numbers from n down to 0."""
     if n < 0:
-        print(n ** 2)
-    do_something(n - 1)
+        return 0
+    print(n ** 2)
+    return do_something(n - 1)
 
 # TODO: 3. write down what you think the output of this will be,
 # TODO: 4. use the debugger to step through and see what's actually happening
-# do_something(4)
+do_something(4)
 
 # TODO: 5. fix do_something() so that it works the way it probably should :)
