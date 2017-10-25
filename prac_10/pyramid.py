535c38f31d69ab0cf7f577f781a22cf271e5c58e
JP Pitman <jeanpaul.pitman@my.jcu.edu.au>
2017-10-25 13:14:38 +1000

    :rocket: add docstrings to pyramid.py

--- prac_10/pyramid.py
+++ prac_10/pyramid.py
@@ -1,15 +1,17 @@
-# def get_block_count(rows):
-#     return (rows * (rows + 1)) / 2
-#
-# blocks = get_block_count(5)
-# print(blocks)
+def get_block_count_a(rows):
+    """Using math"""
+    return (rows * (rows + 1)) // 2
+
+blocks = get_block_count_a(6)
+print(blocks)
 
 
-def get_block_count(rows):
+def get_block_count_b(rows):
+    """using recursion"""
     if rows <= 0:
         return 0
-    return rows + get_block_count(rows - 1)
+    return rows + get_block_count_b(rows - 1)
 
 
-blocks = get_block_count(6)
+blocks = get_block_count_b(6)
 print(blocks)
